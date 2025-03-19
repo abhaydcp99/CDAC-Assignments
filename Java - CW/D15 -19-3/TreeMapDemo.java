b853d11bddc68112b8a9cc8115c2ae135382028b
abhaydcp99 <124073004+abhaydcp99@users.noreply.github.com>
2025-03-19 19:55:20 +0530

    Add files via upload

---
+++ Java - CW/D15 -19-3/TreeMapDemo.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+
+class TreeMapDemo {
+    
+        public static void main(String args[]){
+            Map<Integer , String> m = new TreeMap<><Integer, String>();
+        
+        // <key, values>
+    
+        m.put(101, "Aditya");
+        m.put(2, "dhanush");
+        m.put(3, "shweta");
+        m.put(4, "shweta");      
+        m.put(5, "rohan");
+        m.put(6,"rohini");
+        m.put(7, " ");
+        m.put(8, " ");
+        m.put(9, " ");
+    
+        
+        System.out.println(m);  
+        }
+}
