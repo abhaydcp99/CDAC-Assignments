691afa3af47f62a989d086eb3cdb21fe94602874
abhaydcp99 <124073004+abhaydcp99@users.noreply.github.com>
2025-03-23 10:37:46 +0530

    Update Q9_NumberInRange.java

--- Java Assignment/Assignment -2/Q9_NumberInRange.java
+++ Java Assignment/Assignment -2/Q9_NumberInRange.java
@@ -7,12 +7,12 @@ Q9: Implement a Java program that checks if a number is within
 */
 
 public class Q9_NumberInRange {
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = scanner.nextInt();
-
-        System.out.println("Is number in range (20 to 50): " + (num >= 20 && num <= 50));
+        System.out.println("Enter a number: ");
+        int num =scanner.nextInt();
+        boolean isInRange = (num >= 20 && num <= 50); 
+        System.out.println("Is number in range (20 to 50): " + isInRange);
         scanner.close();
     }
 }
