cac4dff5b0eb03378bfd4a0dd153eb2d8cdab8d3
abhaydcp99 <124073004+abhaydcp99@users.noreply.github.com>
2025-03-26 15:32:34 +0530

    Add files via upload

---
+++ DSA-CW/D2-26-3/NaturalNumber.java
@@ -0,0 +1,15 @@
+// natural number:
+
+public class NaturalNumber {
+    static int sum(int n){
+        if(n>0)
+        return n+sum(n-1);
+        else
+            return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Sum of number: "+sum(10));
+    }
+}
+
+// Sum of number: 55
